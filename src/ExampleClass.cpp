a6497df221cb39e618d40b2961996a68afe56cee
orkzking <thomas.reinirkens@gmail.com>
2019-04-07 11:36:23 +0000

    Dummy implementation of the ExampeClass

---
+++ src/ExampleClass.cpp
@@ -0,0 +1,25 @@
+#include "ExampleClass.h"
+
+public ExampleClass::ExampleClass() {
+    // TODO: implement stuff
+}
+
+public ExampleClass::~ExampleClass() {
+    // TODO: implement stuff
+}
+
+public ExampleClass::ExampleClass(int a, float b) {
+    // TODO: implement stuff
+}
+
+public void ExampleClass::DoSomething() {
+    // TODO: implement stuff
+}
+
+public void ExampleClass::DoNothing() {
+    // TODO: implement stuff
+}
+
+public void* ExampleClass::VeryUsefulMethod(bool level) {
+    // TODO: implement stuff
+}
